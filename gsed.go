624923ddbe912b26755fe674ffcd316def000ce3
Aaron Janse <gitduino@gmail.com>
2018-02-21 19:30:06 -0800

    pressing capslock now fully refreshes rendering

--- gsed.go
+++ gsed.go
@@ -64,7 +64,7 @@ func main() {
 			if cursor.y < len(lines)-1 {
 				lines = append(lines[:cursor.y+1], append([]string{" "}, lines[cursor.y+1:]...)...)
 			} else {
-			lines = append(lines, "")
+				lines = append(lines, "")
 			}
 
 			// clear line after cursor
@@ -151,6 +151,14 @@ func main() {
 			fmt.Fprint(os.Stderr, string(c))
 		case bytes.Equal(c, []byte{9}):
 			fmt.Fprint(os.Stderr, "\t")
+		case bytes.Equal(c, []byte{27}):
+			fmt.Fprintf(os.Stderr, "\033[%vF\033[1E", cursor.y+1)
+			for _, line := range lines {
+				fmt.Fprintln(os.Stderr, line+strings.Repeat(" ", utf8.RuneCountInString(line)))
+			}
+			fmt.Fprint(os.Stderr, "\033[36m~\033[39m"+strings.Repeat(" ", len(lines[len(lines)-1])))
+			fmt.Fprintf(os.Stderr, "\033[%vF", len(lines)-cursor.y)
+			fmt.Fprintf(os.Stderr, "\033[%vG", cursor.x+1)
 		default:
 			// fmt.Fprintln(os.Stderr, "")
 			// fmt.Fprintln(os.Stderr, c)
